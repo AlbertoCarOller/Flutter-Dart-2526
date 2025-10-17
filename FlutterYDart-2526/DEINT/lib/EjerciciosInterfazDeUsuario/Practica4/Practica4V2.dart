3a71021a40ef4d24ee43a188d88727cf922ad34e
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-10-17 12:56:46 +0200

    Ejercicios de interfaz de usuario

---
+++ FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica4/Practica4V2.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Practica4v2());
+}
+
+class Practica4v2 extends StatelessWidget {
+  const Practica4v2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Practiva4V2", home: Dado());
+  }
+}
+
+class Dado extends StatelessWidget {
+  const Dado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              color: Colors.grey,
+              height: 150,
+              width: 150,
+              child: Container(height: 10, width: 10, color: Colors.black),
+            ),
+            Container(
+              color: Colors.white,
+              height: 150,
+              width: 150,
+              padding: EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.grey,
+              height: 150,
+              width: 150,
+              padding: EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 150,
+              width: 150,
+              padding: EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(height: 10, width: 10, color: Colors.black),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica5/Practica5.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Practica4v2());
+}
+
+class Practica4v2 extends StatelessWidget {
+  const Practica4v2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Practiva4V2", home: Dado());
+  }
+}
+
+class Dado extends StatelessWidget {
+  const Dado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Estructura principal
+      body: Column(
+        children: [
+          // Fila de arriba
+          Expanded(
+            child: Row(
+              children: [
+                // Columna en la que se dive el primer cuadro
+                Expanded(
+                  child: Column(
+                    children: [
+                      // Primer cuadro azul
+                      Expanded(
+                        child: Container(
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      // Segundo cuadro verde
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                )
+              ],
+            ),
+          ),
+          // Cuadro rojo de abajo
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
