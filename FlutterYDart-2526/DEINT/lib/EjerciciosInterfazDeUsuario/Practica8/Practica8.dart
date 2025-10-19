5a95eae4418513927bb44c47ea7dbdbcdfe0a16b
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-10-19 16:58:54 +0200

    Ejercicios de interfaz de usuario

--- FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica8/Practica8.dart
+++ FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica8/Practica8.dart
@@ -64,37 +64,200 @@ class Cuadro extends StatelessWidget {
           ),
           Column(
             children: [
-              // Cuadro de filas y columnas, con .count metemos nostros manualmente los hijos
+              // Expanded tiene el tamaño máximo de su padre, no es infinity, así no da problemas para el Grid
               Expanded(
-                child: GridView.count( // TODO: por arreglar la expansión incontrolada del grid
+                /* Cuadro de filas y columnas, con .count metemos nostros manualmente los hijos
+                  * IMPORTANTE: a widgets como este hay que pasarle un tamaño que no sea infinito,
+                  * de lo contrario colapsará */
+                child: GridView.count(
+                  padding: EdgeInsets.only(left: 15, right: 15),
                   // Número de columnas
                   crossAxisCount: 4,
                   // Espacio entre columnas
-                  crossAxisSpacing: 4,
+                  crossAxisSpacing: 5,
                   // Espacio entre filas
-                  mainAxisSpacing: 4,
+                  mainAxisSpacing: 5,
+                  // Para definir el tamaño de cada celda (hijo)
+                  childAspectRatio: 1.5,
                   children: [
                     Text(
                       "Fallout 4",
-                      style: GoogleFonts.fugazOne(color: Colors.amber.shade800),
+                      style: GoogleFonts.fugazOne(
+                        color: Colors.amber.shade800,
+                        fontSize: 20,
+                      ),
                     ),
-                    Container(color: Colors.amberAccent),
-                    Container(color: Colors.amberAccent),
-                    Container(color: Colors.amberAccent),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://i.redd.it/ibu5c31aph601.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://wallpapers.com/images/hd/fallout-4-computer-4hcyao78in7jg4la.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://wallpapers.com/images/hd/diamond-city-1920-x-1080-wallpaper-cgp9mfk2n5nnuszg.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
                     Text(
                       "Fallout New Vegas",
-                      style: GoogleFonts.fugazOne(color: Colors.amber.shade800),
+                      style: GoogleFonts.fugazOne(
+                        color: Colors.amber.shade800,
+                        fontSize: 20,
+                      ),
                     ),
-                    Container(color: Colors.amberAccent),
-                    Container(color: Colors.amberAccent),
-                    Container(color: Colors.amberAccent),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://i.redd.it/fallout-new-vegas-art-in-4k-its-not-ai"
+                            "-upscaled-its-the-v0-0orlyq1b7vyc1.jpg?width=2880&"
+                            "format=pjpg&auto=webp&s=829f3c7062157e63583805e7b9f094acc8ef5fd7",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://i.imgur.com/Z6bPLkN.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://images.fallout.wiki/a/a4/FNV_Location_The_Strip_North_Gate.png",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
                     Text(
                       "Fallout 76",
-                      style: GoogleFonts.fugazOne(color: Colors.amber.shade800),
+                      style: GoogleFonts.fugazOne(
+                        color: Colors.amber.shade800,
+                        fontSize: 20,
+                      ),
                     ),
-                    Container(color: Colors.amberAccent),
-                    Container(color: Colors.amberAccent),
-                    Container(color: Colors.amberAccent),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://cdn.arstechnica.net/wp-content/uploads/2025/10/FO76_BurningSprings_0925-3.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://static0.thegamerimages.com/wordpress/"
+                            "wp-content/uploads/2018/11/fallout-76-emote-1-e1542840866147.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // Con BoxDecoration podemos ponerle bordes al container y demás
+                      decoration: BoxDecoration(
+                        // Colocamos una imagen dentro del container, como fondo
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://assets.newsweek.com/wp-content/uploads/2025/"
+                            "08/1220110-fallout-76-scrap-loot-locations-junk.png?w=1600&quality=75&webp=1",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        // Para poner bordes, en este caso en todos los lados
+                        border: Border.all(
+                          color: Colors.orangeAccent.shade700,
+                          width: 4,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
