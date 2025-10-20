9c6e8c54ab6a4728efcebe39d413c4c3db894100
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-10-20 19:08:52 +0200

    Ejercicios de interfaz de usuario

--- FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica9/Prueba9.dart
+++ FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica9/Prueba9.dart
@@ -33,22 +33,112 @@ class Reporte extends StatelessWidget {
           width: 300,
           height: 450,
           color: Colors.grey.shade200,
+          // Columna principal
           child: Column(
             children: [
               // Zona de arriba
               Expanded(
-                child: Row(
+                child: Column(
                   children: [
-                    // Icono y texto
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(Icons.analytics, color: Colors.blue,),
-                        Text("Reporte Semanal")
+                        // Icono y texto
+                        Row(
+                          children: [
+                            Icon(Icons.analytics, color: Colors.blue),
+                            Text(
+                              "Reporte Semanal",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        // Icono de los 3 puntos
+                        Icon(Icons.more_vert),
                       ],
-                    )
+                    ),
+                    // Linea divisoria
+                    Divider(),
                   ],
                 ),
-              )
+              ),
+              // Estructura de en medio
+              Expanded(
+                flex: 8,
+                child: Row(
+                  children: [
+                    // Primera columna de cuadros azules
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: [
+                          // Primer cuadro azul
+                          Expanded(child: Container(color: Colors.blue)),
+                          // Separación entre cuadros azules
+                          SizedBox(height: 10),
+                          // Segundo cuadro azul
+                          Expanded(
+                            child: Container(color: Colors.blue.shade200),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Separación entre columnas
+                    SizedBox(width: 10),
+                    // Segunda columnas de cuadros verdes
+                    Expanded(
+                      child: Column(
+                        children: [
+                          // Primer cuadro verde
+                          Expanded(
+                            flex: 3,
+                            child: Container(color: Colors.green.shade200),
+                          ),
+                          // Separación
+                          SizedBox(height: 10),
+                          // Segundo cuandro, el más largo
+                          Expanded(
+                            flex: 7,
+                            child: Container(color: Colors.green),
+                          ), // Separación
+                          SizedBox(height: 10),
+                          // Tercer cuadro el más pequeño
+                          Expanded(
+                            child: Container(color: Colors.green.shade100),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Última sección, sección de los botones
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print("Ver detalles");
+                        },
+                        child: Text("Ver detalles"),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print("Exportar");
+                        },
+                        child: Text("Exportar"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -56,4 +146,3 @@ class Reporte extends StatelessWidget {
     );
   }
 }
-
