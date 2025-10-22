ed3cee799125064670acc6ee7abe22aeed3202c7
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-10-22 20:46:30 +0200

    Ejercicios de interfaz de usuario numeros pares

---
+++ FlutterYDart-2526/DEINT/lib/EjerciciosInterfazDeUsuario/Practica12/Practica12.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NumerosPares());
+}
+
+class NumerosPares extends StatelessWidget {
+  const NumerosPares({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Numeros Pares",
+      home: Pares(),
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
+
+class Pares extends StatelessWidget {
+  const Pares({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          // Multiplicamos por 2 para que salgan los números pares
+          return Text("${index * 2}");
+        },
+        separatorBuilder: (context, index) {
+          return Divider(color: Colors.blue,);
+        },
+        itemCount: 100,
+      ),
+    );
+  }
+}
