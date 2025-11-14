9b4263baee1d959b22002839f43e162ff6561ad6
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2025-11-14 12:44:38 +0100

    Ejercicios de GridView

--- FlutterYDart-2526/DEINT/lib/ejercicios_interfaz_usuario/Tarea16/Pantalla3.dart
+++ FlutterYDart-2526/DEINT/lib/ejercicios_interfaz_usuario/Tarea16/Pantalla3.dart
@@ -8,14 +8,23 @@ class Pantalla3 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text("GridView builder")),
       body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        /* SliverGridDelegateWithFixedCrossAxisCount() -> Define el comportamiento,
+         en este caso será como el de un Count */
+        /*gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
           childAspectRatio: 1,
           mainAxisSpacing: 2,
           crossAxisSpacing: 2,
+        ),*/
+        /* SliverGridDelegateWithMaxCrossAxisExtent -> Define el comportamiento,
+         en este caso será como el de un Expanded*/
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
         ),
         itemBuilder: (context, index) {
-          return Container(color: Colors.purple, child: Text("$index"),);
+          return Container(color: Colors.purple, child: Text("$index"));
         },
         itemCount: 200,
       ),
