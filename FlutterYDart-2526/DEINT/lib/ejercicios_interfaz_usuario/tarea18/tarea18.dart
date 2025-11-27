5115c98a82bda37ce950c07ee65ce8b575ffa002
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2025-11-27 19:21:32 +0100

    Ejercicios probando gestures

--- FlutterYDart-2526/DEINT/lib/ejercicios_interfaz_usuario/tarea18/tarea18.dart
+++ FlutterYDart-2526/DEINT/lib/ejercicios_interfaz_usuario/tarea18/tarea18.dart
@@ -9,7 +9,10 @@ class Tarea18 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Tarea18", home: ListaElementos());
+    return MaterialApp(
+      title: "Tarea18",
+      home: ListaElementos(),
+    );
   }
 }
 
@@ -23,24 +26,23 @@ class ListaElementos extends StatefulWidget {
 class _ListaElementosState extends State<ListaElementos> {
   // La varible que va a almacenar el índice del contenedor que va a cambiar de color
   int indexContainer = -1;
+
   // La variable que va a almacenar el índice del usuario, que se inicia con el primer toque del usuario
   late double indexUser;
 
   // Creamos una lista que va a generar 20 containers
-  List get _listaElementos => List.generate(20, (index) {
-    return Container(
-      color: index == indexContainer ? Colors.orangeAccent : Colors.transparent,
-      child: ListTile(
-        title: Text("$index"),
-        trailing: Icon(Icons.accessibility_outlined),
-      ),
-    );
+  final List _listaElementos = List.generate(20, (index) {
+    return "$index";
   });
+
+  // Con una lista de elementos no generados
+  //final List _listaElementos = ["Primero", "Segundo", "Tercero"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.separated(
+        // IMPORTANTE delta DEVUELVE EL TIEMPO DE PULSACIÓN
         itemBuilder: (context, index) {
           // IMPORTANTE: EL ÍNDICE DEL USUARIO NO PUEDE SER NUNCA MENOR A 0 AL COMIENZO
           return GestureDetector(
@@ -59,17 +61,40 @@ class _ListaElementosState extends State<ListaElementos> {
             * ya que 0 es la posición inical, izquierda números negativos y derecha
             * números positivos como el eje x */
             onHorizontalDragUpdate: (details) {
-              if ((details.localPosition.dx - indexUser) < -100) {
+              if ((details.localPosition.dx - indexUser) /*< -100*/ > 150) {
                 setState(() {
                   indexContainer = index;
                 });
               }
             },
-            child: _listaElementos.elementAt(index),
+            /* Esta función se queda con la última posición donde pulso el usario,
+            * podemos obtener este posición, en mi caso lo hago para cuando el usuario
+            * arrastre en esa posición elimine ese mismo container */
+            onHorizontalDragEnd: (details) {
+              if ((details.localPosition.dx - indexUser) < -150) {
+                setState(() {
+                  _listaElementos.removeAt(indexContainer);
+                  indexContainer =
+                      -1; /* -> Quitamos el índice residual después de eliminar
+                   el elemento (container) porque si por ejemplo pintamos el elemento con
+                   índice 5 y lo eliminamos el elemento 6 antes de eliminar pasará a
+                   ser el elemento 5 y estará pintado*/
+                });
+              }
+            },
+            // El elemento de la lista
+            child: Container(
+              // Si el índice del container coindice con el índice del actual cambia de color
+              color: index == indexContainer ? Colors.red : Colors.transparent,
+              child: ListTile(
+                title: Text("${_listaElementos[index]}"),
+                trailing: Icon(Icons.accessibility_outlined),
+              ),
+            ),
           );
         },
         separatorBuilder: (context, index) => Divider(),
-        itemCount: 20,
+        itemCount: _listaElementos.length,
       ),
     );
   }
