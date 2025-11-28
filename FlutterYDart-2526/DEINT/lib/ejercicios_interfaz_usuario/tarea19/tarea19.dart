ec1ae886f452f8f17bc76ad7a0f40c23e69feea4
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2025-11-28 10:17:25 +0100

    Ejercicios Dismissible probando

---
+++ FlutterYDart-2526/DEINT/lib/ejercicios_interfaz_usuario/tarea19/tarea19.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Tarea19());
+}
+
+class Tarea19 extends StatelessWidget {
+  const Tarea19({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Tarea19", home: TareasBorrar());
+  }
+}
+
+class TareasBorrar extends StatefulWidget {
+  const TareasBorrar({super.key});
+
+  @override
+  State<TareasBorrar> createState() => _TareasBorrarState();
+}
+
+class _TareasBorrarState extends State<TareasBorrar> {
+  final _listaTareas = List.generate(20, (index) => "Tarea $index");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          /* Dismissible -> Este widget tiene la funcionalidad de arrastrar el elemento
+          * a la dirección que queramos para eliminar un elemento, no es necesario controlar
+          * los índices, se le puede editar la dirección y el color de fondo al eliminar */
+          return Dismissible(
+            // Función para el borrado
+            onDismissed: (direction) {
+              setState(() {
+                if (direction == DismissDirection.endToStart) {
+                  _listaTareas.removeAt(index);
+                }
+              });
+            },
+            background: Container(color: Colors.red),
+            /*direction: DismissDirection.endToStart,*/
+            key: /*UniqueKey() -> Key única que genera flutter, vamos a poner una clave
+            que sea directamente el elemento para que no dé error, si ponemos un int da 
+            error*/ ValueKey(
+              _listaTareas.elementAt(index),
+            ),
+            child: ListTile(
+              title: Text(_listaTareas.elementAt(index)),
+              trailing: Icon(Icons.work),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: _listaTareas.length,
+      ),
+    );
+  }
+}
