e3115487f4bc51c93df655ac59bf92d419a41265
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2025-12-22 20:19:51 +0100

    Ejercicio del tiempo, funcionalidad por ubicación actual completa

---
+++ prueba_geolocalizador/android/app/src/main/kotlin/com/example/prueba_geolocalizador/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prueba_geolocalizador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- weather_app/lib/screens/principal_screen.dart
+++ weather_app/lib/screens/principal_screen.dart
@@ -6,7 +6,8 @@ import 'package:geocoding/geocoding.dart'; /* -> Importamos el paquete para obte
  porque puede haber varias ciudades que se llamen igual en diferentes países, la primera suele ser la
  más famosa*/
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart'
+    as http; // -> Para acceder a las APIs, en este caso la del tiempo
 import 'package:weather_app/model/weather.dart'; // Para obtener la ubicación actual
 
 class PrincipalScreen extends StatefulWidget {
@@ -38,10 +39,14 @@ class _PrincipalScreenState extends State<PrincipalScreen> {
   // Creamos un índice, este recoge la hora seleccionada por el usuario
   int indexSeleccion = 0;
 
+  // Creamos un scrollController
+  ScrollController sc = ScrollController();
+
   @override
   void initState() {
     super.initState();
     _textUbicaion.addListener(() => print(_textUbicaion.text));
+    sc.addListener(() => print("${sc.position.pixels}"));
     // Se pueden utilizar funciones asíncronas pero no darle valor a una variable si viene de una
     cargarDatosActual();
   }
@@ -49,6 +54,7 @@ class _PrincipalScreenState extends State<PrincipalScreen> {
   @override
   void dispose() {
     _textUbicaion.dispose();
+    sc.dispose();
     super.dispose();
   }
 
@@ -76,33 +82,64 @@ class _PrincipalScreenState extends State<PrincipalScreen> {
         ),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Text("Tu ubicación"), Icon(Icons.location_on)],
-                  ),
-                  // La ubicación para saber el tiempo (nombre de la ciudad)
-                  Text(
-                    ubicaion == null ? "Por determinar" : ubicaion!,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  // Obtenemos la fecha actual
-                  Text(
-                    "${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}",
-                  ),
-                  Padding(
-                    padding: EdgeInsetsGeometry.symmetric(vertical: 40),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        // Imagen dependiendo de si está nublado en el momento o no
-                        Expanded(
-                          child: Image.asset(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("Tu ubicación"), Icon(Icons.location_on)],
+              ),
+              // La ubicación para saber el tiempo (nombre de la ciudad)
+              Text(
+                ubicaion == null ? "Por determinar" : ubicaion!,
+                style: TextStyle(fontSize: 20),
+              ),
+              // Obtenemos la fecha actual
+              Text(
+                "${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}",
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(vertical: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    // Imagen dependiendo de si está nublado en el momento o no
+                    Expanded(
+                      child: Image.asset(
+                        weather != null
+                            ? (weather!.hourly!.cloudCover.elementAt(
+                                            indexSeleccion,
+                                          ) >=
+                                          50 &&
+                                      weather!.hourly!.cloudCover.elementAt(
+                                            indexSeleccion,
+                                          ) <
+                                          100
+                                  ? "assets/images/parcialmenteNublado.png"
+                                  : weather!.hourly!.cloudCover.elementAt(
+                                          indexSeleccion,
+                                        ) ==
+                                        100
+                                  ? "assets/images/nublado.png"
+                                  : "assets/images/soleado.png")
+                            : "assets/images/soleado.png",
+                        scale: 3,
+                      ),
+                    ),
+                    // Columna que contiene los grados y debajo de esta si está nublado y demás
+                    Expanded(
+                      child: Column(
+                        children: [
+                          // Grados
+                          Text(
+                            weather != null
+                                ? "${weather!.hourly!.temperature2M.elementAt(indexSeleccion)}°C"
+                                : "_Cº",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          // 'Parcialmente nublado', 'Nublado' o 'Soleado'
+                          Text(
                             weather != null
                                 ? (weather!.hourly!.cloudCover.elementAt(
                                                 indexSeleccion,
@@ -112,67 +149,67 @@ class _PrincipalScreenState extends State<PrincipalScreen> {
                                                 indexSeleccion,
                                               ) <
                                               100
-                                      ? "assets/images/parcialmenteNublado.png"
+                                      ? "Parcialmente nublado"
                                       : weather!.hourly!.cloudCover.elementAt(
                                               indexSeleccion,
                                             ) ==
                                             100
-                                      ? "assets/images/nublado.png"
-                                      : "assets/images/soleado.png")
-                                : "assets/images/soleado.png",
+                                      ? "Nublado"
+                                      : "Soleado")
+                                : "Por determinar",
+                            style: TextStyle(fontSize: 20),
                           ),
-                        ),
-                        // Columna que contiene los grados y debajo de esta si está nublado y demás
-                        Expanded(
-                          child: Column(
-                            children: [
-                              // Grados
-                              Text(
-                                weather != null
-                                    ? "${weather!.hourly!.temperature2M.elementAt(indexSeleccion)}°C"
-                                    : "_Cº",
-                                style: TextStyle(fontSize: 30),
-                              ),
-                              // 'Parcialmente nublado', 'Nublado' o 'Soleado'
-                              Text(
-                                weather != null
-                                    ? (weather!.hourly!.cloudCover.elementAt(
-                                                    indexSeleccion,
-                                                  ) >=
-                                                  50 &&
-                                              weather!.hourly!.cloudCover
-                                                      .elementAt(
-                                                        indexSeleccion,
-                                                      ) <
-                                                  100
-                                          ? "Parcialmente nublado"
-                                          : weather!.hourly!.cloudCover
-                                                    .elementAt(
-                                                      indexSeleccion,
-                                                    ) ==
-                                                100
-                                          ? "Nublado"
-                                          : "Soleado")
-                                    : "Por determinar",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  // Row donde van las 4 cards de información básica
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 40),
-                    child: Row(children: []),
-                  ),
-                  // Row Scrollable con las cards de las horas
-                  Row(),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              // Row donde van las 4 cards de información básica
+              Padding(
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    for (int i = 0; i < 4; i++)
+                      // La card personalizada en caso de que 'weather no sea null'
+                      weather != null
+                          ? CardPersonalizada(
+                              indexFor: i,
+                              weather: weather!,
+                              indexUser: indexSeleccion,
+                            )
+                          : Text("No disponible"),
+                  ],
+                ),
+              ),
+              // Este es el apartado donde aparecen las cards de las horas
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                controller: sc,
+                child: Row(
+                  children: [
+                    for (int i = 0; i < 24; i++)
+                      weather != null
+                          ? GestureDetector(
+                              // Al pulsar cambiamos por el índice que está seleccionado
+                              onTap: () {
+                                setState(() {
+                                  indexSeleccion = i;
+                                });
+                              },
+                              child: CardHora(
+                                indexFor: i,
+                                weather: weather!,
+                                currentIndex: indexSeleccion,
+                              ),
+                            )
+                          : Text("No disponible "),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -307,10 +344,24 @@ Future<Weather?> cargarWeatherActual(Position position) async {
   }
 }
 
+/// Este widget va a tener una columna con la información, incluyendo la card
+/// de cada sección que se necesita saber del tiempo
 class CardPersonalizada extends StatelessWidget {
-  // El índice del for, se lo pasamos para saber que va en la card
+  // Pasamos el índice que va a representar que hora ha elegido el usurio
+  final int indexUser;
+
+  // El ídice del for, el que llega desde arriba, para saber que mostrar
   final int indexFor;
-  const CardPersonalizada({super.key, required this.indexFor});
+
+  // Alamacenamos el objeto Weather
+  final Weather weather;
+
+  const CardPersonalizada({
+    super.key,
+    required this.indexFor,
+    required this.weather,
+    required this.indexUser,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -319,9 +370,110 @@ class CardPersonalizada extends StatelessWidget {
       children: [
         Card(
           color: Colors.grey.shade400,
-          child: SizedBox(width: 50, height: 50),
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            // La imagen dependiendo del índice de del for
+            child: Image.asset(
+              indexFor == 0
+                  ? "assets/images/termometro.png"
+                  : indexFor == 1
+                  ? "assets/images/nublado.png"
+                  : indexFor == 2
+                  ? "assets/images/gotaAgua.png"
+                  : "assets/images/viento.png",
+              scale: 15,
+            ),
+          ),
+        ),
+        // Texto que aparece justo debajo de la card
+        Text(
+          indexFor == 0
+              ? "Mínima-máxima"
+              : indexFor == 1
+              ? "Nubosidad"
+              : indexFor == 2
+              ? "Precipitaciones"
+              : "Viento",
+        ),
+        // Texto que contiene la información
+        Text(
+          indexFor == 0
+              /* Con RegExp(r'[\[\]]') creamos una expresión regular, en este caso con 'r'
+          * le decimos que las \ sean para escapar y no haya que poner doble \\ */
+              ? "${weather.daily!.temperature2MMin.toString().split(RegExp(r'[\[\]]')).elementAt(1)}º"
+                    "-${weather.daily!.temperature2MMax.toString().split(RegExp(r'[\[\]]')).elementAt(1)}º"
+              : indexFor == 1
+              ? "${weather.hourly!.cloudCover.elementAt(indexUser)}%"
+              : indexFor == 2
+              ? "${weather.hourly!.precipitationProbability.elementAt(indexUser)}"
+              : "${weather.hourly!.windSpeed10M.elementAt(indexUser)} km/h",
         ),
       ],
+    );
+  }
+}
+
+class CardHora extends StatelessWidget {
+  // El índice del for exterior
+  final int indexFor;
+
+  // El objeto weather
+  final Weather weather;
+
+  // El índice seleccionado por el usuario, para saber de que color debe estar la card
+  final int currentIndex;
+
+  const CardHora({
+    super.key,
+    required this.indexFor,
+    required this.weather,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: currentIndex == indexFor
+          ? Colors.purple.shade400
+          : Colors.grey.shade400,
+      child: SizedBox(
+        height: 110,
+        width: 70,
+        child: Stack(
+          children: [
+            // La hora
+            Positioned(
+              top: 4,
+              right: 2,
+              left: 2,
+              child: Text(
+                weather.hourly!.time
+                    .elementAt(indexFor)
+                    .toString()
+                    .split("T")
+                    .elementAt(1),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Positioned(
+              left: 2,
+              right: 2,
+              bottom: 2,
+              top: 2,
+              child: Image.asset(
+                weather.hourly!.cloudCover.elementAt(indexFor) >= 50 &&
+                        weather.hourly!.cloudCover.elementAt(indexFor) < 100
+                    ? "assets/images/parcialmenteNublado.png"
+                    : weather.hourly!.cloudCover.elementAt(indexFor) == 100
+                    ? "assets/images/nublado.png"
+                    : "assets/images/soleado.png",
+                scale: 12,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
